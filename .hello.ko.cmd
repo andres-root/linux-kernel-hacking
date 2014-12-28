199ad93d4b761d81853b8ba53265e09bf941e9ca
andres-root <andres.root.coder@gmail.com>
2014-12-27 21:45:26 -0500

    module tested. its working

---
+++ .hello.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/andresroot/sites/linux/linux-kernel-hacking/hello.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/andresroot/sites/linux/linux-kernel-hacking/hello.ko /home/andresroot/sites/linux/linux-kernel-hacking/hello.o /home/andresroot/sites/linux/linux-kernel-hacking/hello.mod.o
---
+++ .tmp_versions/hello.mod
@@ -0,0 +1,2 @@
+/home/andresroot/sites/linux/linux-kernel-hacking/hello.ko
+/home/andresroot/sites/linux/linux-kernel-hacking/hello.o
