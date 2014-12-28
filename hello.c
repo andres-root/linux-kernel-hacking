11b901dfa2b7b626332a427d4ccdc8b5da538958
andres-root <andres.root.coder@gmail.com>
2014-12-27 21:32:27 -0500

    adding cleanup and init function

--- hello.c
+++ hello.c
@@ -6,7 +6,6 @@ MODULE_LICENCE("GPL");
 MODULE_AUTHOR("andresroot");
 MODULE_DESCRIPTION("A hello world module");
 
-
 static int __init hello_init(void)
 {
 	printk(KERN_INFO "Hello world! module by andresroot.\n");
